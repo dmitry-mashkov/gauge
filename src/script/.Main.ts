a09fe3464e29d2da25b229942f5af5fd84b1fed2
dmitry-mashkov <dmitry-mashkov@bitbucket.org>
2016-07-13 14:08:29 +0300

    Working on Gauge. Scale implemented

--- src/script/.Main.ts
+++ src/script/.Main.ts
@@ -1,4 +1,5 @@
 /// <reference path="./CSSClasses.ts"/>
+/// <reference path="./SVGHelper.ts"/>
 
 /// <reference path="./DrawingContext.ts"/>
 /// <reference path="./Gauge.ts"/>
--- src/script/Gauge.ts
+++ src/script/Gauge.ts
@@ -15,6 +15,17 @@ module Gauge
         constructor()
         {
             //super(['OnShown', 'OnClosed']);
+            this.sectors = [
+                {
+                    color: 'green'
+                },
+                {
+                    color: 'yellow'
+                },
+                {
+                    color: 'red'
+                }
+            ]
         }
 
 
@@ -24,6 +35,11 @@ module Gauge
             return this.isReadOnly;
         }
 
+        /**
+         * Value in percentage
+         *
+         * @type {number}
+         */
         private value: number = 0;
         public get Value(): number
         {
@@ -31,17 +47,65 @@ module Gauge
         }
         public set Value(v: number)
         {
-            if (v !== this.value && v > 0 && v < 180)
+            if (v !== this.value && v >= 0 && v <= 100)
             {
                 this.value = v;
-                this.onValueChanged();
+                this.redrawPointer();
             }
+        }
+
+        private labels: Array = [1, 2, 3, 4, 5, 6];
+        public get Labels(): Array
+        {
+            return this.labels;
+        }
+        public set Labels(v: Array)
+        {
+            this.labels = v;
+            this.redrawCanvas();
+        }
+
+        private sectors: Array = [];
+        public get Sectors(): Array
+        {
+            return this.sectors;
+        }
+        public set Sectors(v: Array)
+        {
+            this.sectors = v;
+            this.redrawCanvas();
         }
 
         private drawingContext: DrawingContext = null;
         public get DrawingContext(): DrawingContext
         {
             return this.drawingContext;
+        }
+
+        private availableDegrees: number = 260;
+        public get AvailableDegrees(): number
+        {
+            return this.availableDegrees;
+        }
+        public set AvailableDegrees(v: number)
+        {
+            if (this.availableDegrees !== v)
+            {
+                this.availableDegrees = v;
+                this.redrawCanvas();
+            }
+        }
+
+        private _svgCanvas: Element = null;
+        private get svgCanvas(): Element
+        {
+            if (this._svgCanvas === null)
+            {
+                this._svgCanvas = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                this._svgCanvas.setAttribute('width', '325');
+                this._svgCanvas.setAttribute('height', '325');
+            }
+            return this._svgCanvas;
         }
 
 
@@ -80,47 +144,76 @@ module Gauge
         }
 
 
-        private redrawCanvas()
+        private redrawCanvas(force = false)
         {
+            if (!force && this.DrawingContext === null)
+                return;
+
             // Clear canvas
             while (this.canvasElement.firstChild) {
                 this.canvasElement.removeChild(this.canvasElement.firstChild);
             }
 
-            var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-                path = document.createElementNS('http://www.w3.org/2000/svg', 'path'),
-                text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            var sectorDuration = this.availableDegrees / this.sectors.length,
+                startPoint = 360 - this.availableDegrees / 2,
+                fakeScale = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
-            // Draw arc
-            path.setAttribute('d', "M180 180 A 50 50 0 0 0 20 180");
-            path['style']['stroke'] = 'green';
-            path['style']['fill'] = 'transparent';
-            path['style']['stroke-width'] = '3';
+            // Fake scale is done in sake of labels binding
+            fakeScale.setAttribute('d', SVGHelper.DescribeArc(150, 150, 110, 230, 130));
+            fakeScale.setAttribute('id', 'fake_scale');
+            fakeScale['style']['fill'] = 'transparent';
+            fakeScale['style']['stroke'] = 'black';
+            this.svgCanvas.appendChild(fakeScale);
 
-            // Set numbers
-            //<text x="10" y="10">Hello World!</text>
-            text.setAttribute('x', '20');
-            text.setAttribute('y', '20');
-            text.textContent = "Hello";
 
-            svg.setAttribute('width', '325');
-            svg.setAttribute('height', '325');
-            svg.appendChild(path);
-            svg.appendChild(text);
+            for (var i in this.sectors)
+            {
+                var p = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                var start = i * sectorDuration + startPoint;
+                if (start > 360)
+                    start -= 360;
+                var end = start + sectorDuration;
+                if (end > 360)
+                    end -= 360;
 
-            this.canvasElement.appendChild(svg);
+                // Draw a sector
+                p.setAttribute('d', SVGHelper.DescribeArc(150, 150, 100, start, end));
+                p['style']['stroke'] = this.sectors[i].color;
+                p['style']['fill'] = 'transparent';
+                p['style']['stroke-width'] = '3';
 
+                this.svgCanvas.appendChild(p);
+            }
+
+
+            for (var i in this.labels)
+            {
+                var l = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+                //l.setAttribute('x', '20');
+                //l.setAttribute('y', '20');
+                //l.textContent = this.labels[i];
+
+                var t = document.createElementNS('http://www.w3.org/2000/svg', 'textPath');
+                t.setAttribute('xlink:href', '#fake_scale');
+
+                l.appendChild(t);
+
+                this.svgCanvas.appendChild(l);
+            }
+
+            this.canvasElement.appendChild(this.svgCanvas);
         }
 
-        private redrawPointer()
+        private redrawPointer(force = false)
         {
+            if (!force && this.DrawingContext === null)
+                return;
 
-        }
+            var p = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            p.setAttribute('d', 'M230 80 A 45 45, 0, 1, 0, 275 125 L 275 80 Z');
+            p['style']['fill'] = 'blue';
 
-        private onValueChanged()
-        {
-            if (this.DrawingContext !== null)
-                this.redrawPointer();
+            this.svgCanvas.appendChild(p);
         }
 
     }
